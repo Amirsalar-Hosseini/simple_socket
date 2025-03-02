e6e885ceba2642a0ee108c0d03e8c4445e566529
amirsalar-hosseini <hosseiniseyedamirsalar@gmail.com>
2025-03-02 21:15:16 +0330

    simple transfer data between server and client was created

---
+++ server.py
@@ -0,0 +1,33 @@
+import socket
+
+
+
+server_ip = "127.0.0.1"
+server_port = 8000
+
+
+def run_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((server_ip, server_port))
+    server.listen(0)
+    print(f"listening on {server_ip}: {server_port}")
+
+    client_socket, client_address = server.accept()
+    print(f"accepted connection from {client_address[0]}: {client_address[1]}")
+
+
+    while True:
+        request = client_socket.recv(1024)
+        request = request.decode("utf-8")
+
+        if request.lower() == "close":
+            client_socket.send("closed".encode("utf-8"))
+            break
+
+        print(f"received : {request}")
+        client_socket.send("accenpted".encode("utf-8"))
+
+    client_socket.close()
+    server.close()
+
+run_server()
